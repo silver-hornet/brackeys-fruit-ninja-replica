a8f2b38606c9960171489768c1f8469e1a51cb6b
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-01-13 10:20:57 -0500

    Completed tutorial

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    [SerializeField] GameObject fruitSlicedPrefab;
+    Rigidbody2D rb;
+    [SerializeField] float startForce = 15f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up * startForce, ForceMode2D.Impulse);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Blade"))
+        {
+            Vector3 direction = (collision.transform.position - transform.position).normalized;
+            Quaternion rotation = Quaternion.LookRotation(direction);
+            GameObject slicedFruit = Instantiate(fruitSlicedPrefab, transform.position, rotation);
+            Destroy(slicedFruit, 3f);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/FruitSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject fruitPrefab;
+    [SerializeField] Transform[] spawnPoints;
+    [SerializeField] float minDelay = 0.1f;
+    [SerializeField] float maxDelay = 1f;
+
+    void Start()
+    {
+        StartCoroutine(SpawnFruits());
+    }
+
+    IEnumerator SpawnFruits()
+    {
+        while (true)
+        {
+            float delay = Random.Range(minDelay, maxDelay);
+            yield return new WaitForSeconds(delay);
+            int spawnIndex = Random.Range(0, spawnPoints.Length);
+            Transform spawnPoint = spawnPoints[spawnIndex];
+            GameObject spawnedFruit = Instantiate(fruitPrefab, spawnPoint.position, spawnPoint.rotation);
+            Destroy(spawnedFruit, 5f);
+        }    
+    }
+}
